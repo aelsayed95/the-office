493ca8321cb1b43becbade7867c62141a28cf989
Aya Elsayed <ayah.ehab11@gmail.com>
2022-09-10 13:43:01 +0100

    4 - Documenting your API

--- employee.py
+++ employee.py
@@ -1,20 +1,52 @@
+from dataclasses import dataclass
+from datetime import date
 import uuid
 
 JOB_TITLES = ["SALES_REP", "REGIONAL_MANAGER", "MANAGER", "HR", "ACCOUNTING"]
+""":obj:`list` of :obj:`str`: A list of valid job titles at Dunder Mifflin. 
+"""
 
 
+@dataclass
 class Employee:
+    """Class for keeping track of employee details.
+
+    Args:
+        name (str): full name of :obj:`Employee`
+        job_title (str): job title
+
+    Attributes:
+        employee_id (str): a unique :obj:`Employee` id - generated on construction
+    """
+
     def __init__(self, name: str, job_title: str):
         self.name = name
         self.job_title = job_title
         self.employee_id = uuid.uuid4()
 
     def update_contact_info(self, name: str) -> str:
+        """Update :obj:`Employee`'s contact inforamtion
+
+        Args:
+            new_name (str): new :obj:`Employee`'s name
+
+        Returns:
+            str: :obj:`Employee`'s old name
+        """
         past_name = self.name
         self.name = name
         return past_name
 
     def promote_employee(self, job_title):
+        """Assigns a new job title to :obj:`Employee`
+
+        Args:
+            job_title (str): the new job title
+
+        Raises:
+            ValueError: If the new job title is not valid
+        """
+
         if job_title not in JOB_TITLES:
             raise ValueError(
                 f"Invalid job title {job_title}, expected one of {JOB_TITLES}"
--- office.py
+++ office.py
@@ -5,6 +5,14 @@ from employee import Employee
 
 
 class TheOffice:
+    """A class representing an office that holds information about its employees and their sales.
+    It exposes methods make and view sales for each employee.
+
+    Args:
+        regional_manager (str): The name of the employee.
+        employees (Set[Employee]): A set of employee objects.
+        location (str): The name of the region the office is located.
+    """
 
     def __init__(self, regional_manager: str, employees: Set[Employee], location: str):
         self.regional_manager = regional_manager
@@ -13,13 +21,40 @@ class TheOffice:
         self.sales = defaultdict(int)
 
     def __str__(self) -> str:
+        """Represents the class object as a string providing a welcome message
+        and the name of the `regional_manager`.
+
+        Returns:
+            str: Welcome message including the name of the `regional_manager`.
+        """
         return f"Welcome to The Office! The boss here is {self.regional_manager}."
 
     def record_sale(self, employee: Employee) -> None:
+        """Method that adds a sale to the employee key of the sales dict.
+
+        Args:
+            employee (Employee): The `employee` who has made the sale.
+        """
         self.sales[employee] += 1
 
     def get_employee_sales(self, employee: Employee) -> int:
+        """Method that returns the number of sales given an employee.
+
+        Args:
+            employee (Employee): The employee whose sales number we are interested in.
+
+        Returns:
+            int: The total number of sales the employee has made.
+        """
         return self.sales[employee]
 
     def _fire_employee(self, employee: Employee) -> None:
+        """Remove the `employee` from the `employees` set of the class object.
+
+        Args:
+            employee (Employee): The `employee` who is getting deleted.
+
+        Raises:
+            KeyError: If `employee` is not found in the set of `employees`.
+        """
         self.employees.remove(employee)
